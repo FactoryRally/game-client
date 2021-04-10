0dc348785d36be38b9d54cab63e1aebddccab64e
Nils Brugger <nbrugger@student.tgm.ac.at>
2021-04-10 21:25:43 +0200

    refactor: replace 2D array with 1D array
    The 2D array was replaced by a 1d array because Newtonsoft can't serialize
    2D arrays.
    1) Added this indexer for 2D mapping

--- RoboRally/Assets/_Project/Scripts/Utils/Map.cs
+++ RoboRally/Assets/_Project/Scripts/Utils/Map.cs
@@ -6,210 +6,233 @@ using System.Runtime.Serialization;
 
 [DataContract]
 public class Map {
-
-	[DataMember]
-	private Tile[,] Tiles; // Columns | Rows => Tiles[0][2] = 0 | 2
-
-	[DataMember]
-	public int columnCount {
-		get {
-			return ColumnCount;
-		}
-		set { }
-	}
-	private int ColumnCount = 10;
-
-	[DataMember]
-	public int rowCount {
-		get {
-			return RowCount;
-		}
-		set { }
-	}
-	private int RowCount = 10;
+	private              GameLogic _game;
 
 
-	public Tile this[int c, int r] {
-		get { return Tiles[c, r]; }
-		set {
-			if(value == null) {
-				Tiles[c, r] = value;
-				return;
-			}
-			if(value.Type == TileType.PrioCore && PrioCoreCount() > 0)
-				throw new ArgumentException("Only one Prio Core per Map allowed");
-			Tiles[c, r] = value;
-		}
-	}
+	//This is mapped to a 1d array because KALIAN FUCKED UP AGAIN
+	[DataMember] private Tile[]    _tiles;
 
 	public Map(int columnCount = 10, int rowCount = 10) {
-		Tiles = GetEmptyMap(columnCount, rowCount);
+		Height = columnCount;
+		Width  = rowCount;
+		_tiles = InitEmpty(columnCount, rowCount);
 	}
 
-	public Tile[,] GetEmptyMap(int columnCount, int rowCount) {
-		this.ColumnCount = columnCount;
-		this.RowCount = rowCount;
-		return GetEmptyMap();
+	[field: DataMember] public int Height { get; private set; }
+
+	[field: DataMember] public int Width { get; private set; }
+
+	public MapInfo Info => new MapInfo(this);
+
+	public void Assign(GameLogic logic) {
+		_game = logic;
 	}
-
-	public Tile[,] GetEmptyMap() {
-		Tile[,] tiles = new Tile[ColumnCount, RowCount];
-
-		for(int c = 0; c < ColumnCount; c++) {
-			for(int r = 0; r < RowCount; r++) {
-				tiles[c, r] = new Tile();
+	public Position PrioCorePos {
+		get {
+			for (int y = 0; y < Width; y++) {
+				for (int x = 0; x < Height; x++) {
+					if (this[x, y].Type == TileType.PrioCore)
+						return new Position(x, y);
+				}
 			}
+			return null;
 		}
+	}
 
+
+	/// <summary>
+	/// Set/Get the Tile at the specific position defined by X and Y cordinates
+	/// </summary>
+	/// <param name="x">the x cordinate (starting at 0)</param>
+	/// <param name="y">the y cordinate (starting at 0)</param>
+	/// <exception cref="ArgumentException">When trying to set an illegal tile (eg. 2 prio cores on one map)</exception>
+	public Tile this[int x, int y] {
+		get => _tiles[x+y*Width];
+		set {
+			if (value.Type == TileType.PrioCore && PrioCoreCount > 0)
+				throw new ArgumentException("Only one Prio Core per Map allowed");
+			_tiles[x+y*Width] = value;
+		}
+	}
+
+	public int PrioCoreCount {
+		get {
+			int count = 0;
+			for (int c = 0; c < Height; c++) {
+				for (int r = 0; r < Width; r++) {
+					if (this[c, r].Type == TileType.PrioCore)
+						count++;
+				}
+			}
+
+			return count;
+		}
+	}
+
+	private static Tile[] InitEmpty(int columnCount, int rowCount) {
+		Tile[] tiles = new Tile[columnCount * rowCount];
+		for (int c = 0; c < columnCount; c++) {
+			for (int r = 0; r < rowCount; r++) tiles[r * columnCount + c] = new Tile();
+		}
 		return tiles;
 	}
 
 
+	/// <summary>
+	/// Caculates wich fields are empty (no entities on top) and sets the regarding property
+	/// </summary>
+	public void CalculateEmpty() {
+		if(_game == null)
+			return;
+		ImmutableList<(int X, int Y)> occupied = _game.Entitys.List
+													  .Select(selector: e => e.Location)
+													  .Select(selector: p => (p.X, p.Y))
+													  .ToImmutableList();
+		for (int c = 0; c < Height; c++) {
+			for (int r = 0; r < Width; r++) this[c, r].Empty = !occupied.Contains((c, r));
+		}
+	}
+
 	public bool AddColumn(int index) {
-		if(index < 0 || index > ColumnCount)
+		if (index < 0 || index > Height)
 			return false;
 
-		ColumnCount++;
-		Tile[,] tiles = new Tile[ColumnCount, RowCount];
-		for(int c = 0; c < ColumnCount; c++) {
-			for(int r = 0; r < RowCount; r++) {
-				if(c < index) {
-					tiles[c, r] = Tiles[c, r];
-				} else if(c > index) {
-					tiles[c, r] = Tiles[c - 1, r];
-				} else {
+		Height++;
+		Tile[,] tiles = new Tile[Height, Width];
+		for (int c = 0; c < Height; c++) {
+			for (int r = 0; r < Width; r++) {
+				if (c < index)
+					tiles[c, r] = this[c, r];
+				else if (c > index)
+					tiles[c, r] = this[c - 1, r];
+				else
 					tiles[c, r] = new Tile();
-				}
 			}
 		}
-		Tiles = tiles;
+
+		SetDeepTiles(tiles);
 
 		return true;
 	}
 
 	public bool RemoveColumn(int index) {
-		if(index < 0 || index >= ColumnCount)
+		if (index < 0 || index >= Height)
 			return false;
 
-		ColumnCount--;
-		Tile[,] tiles = new Tile[ColumnCount, RowCount];
-		for(int c = 0; c < ColumnCount; c++) {
-			for(int r = 0; r < RowCount; r++) {
-				if(c < index) {
-					tiles[c, r] = Tiles[c, r];
-				} else if(c > index) {
-					tiles[c, r] = Tiles[c + 1, r];
-				} else {
+		Height--;
+		Tile[,] tiles = new Tile[Height, Width];
+		for (int c = 0; c < Height; c++) {
+			for (int r = 0; r < Width; r++) {
+				if (c < index)
+					tiles[c, r] = this[c, r];
+				else if (c > index)
+					tiles[c, r] = this[c + 1, r];
+				else
 					tiles[c, r] = new Tile();
-				}
 			}
 		}
-		Tiles = tiles;
+
+		SetDeepTiles(tiles);
 
 		return true;
 	}
 
-	public bool AddRow(int index) {
-		if(index < 0 || index > RowCount)
-			return false;
-
-		RowCount++;
-		Tile[,] tiles = new Tile[ColumnCount, RowCount];
-		for(int c = 0; c < ColumnCount; c++) {
-			for(int r = 0; r < RowCount; r++) {
-				if(r < index) {
-					tiles[c, r] = Tiles[c, r];
-				} else if(r > index) {
-					tiles[c, r] = Tiles[c, r - 1];
-				} else {
-					tiles[c, r] = new Tile();
-				}
+	private void SetDeepTiles(Tile[,] tiles) {
+		Height = tiles.GetLength(1);
+		Width  = tiles.GetLength(0);
+		_tiles = new Tile[Height*Width];
+		for (int y = 0; y < Height; y++) {
+			for (int x = 0; x < Width; x++) {
+				this[x, y] = tiles[x, y];
 			}
 		}
-		Tiles = tiles;
+	}
+
+	public bool AddRow(int index) {
+		if (index < 0 || index > Width)
+			return false;
+
+		Width++;
+		Tile[,] tiles = new Tile[Height, Width];
+		for (int c = 0; c < Height; c++) {
+			for (int r = 0; r < Width; r++) {
+				if (r < index)
+					tiles[c, r] = this[c, r];
+				else if (r > index)
+					tiles[c, r] = this[c, r - 1];
+				else
+					tiles[c, r] = new Tile();
+			}
+		}
+
+		SetDeepTiles(tiles);
 
 		return true;
 	}
 
 	public bool RemoveRow(int index) {
-		if(index < 0 || index >= RowCount)
+		if (index < 0 || index >= Width)
 			return false;
 
-		RowCount--;
-		Tile[,] tiles = new Tile[ColumnCount, RowCount];
-		for(int c = 0; c < ColumnCount; c++) {
-			for(int r = 0; r < RowCount; r++) {
-				if(r < index) {
-					tiles[c, r] = Tiles[c, r];
-				} else if(r > index) {
-					tiles[c, r] = Tiles[c, r + 1];
-				} else {
-					tiles[c, r] = new Tile();
-				}
+		Width--;
+		Tile[,] tiles = new Tile[Height, Width];
+		for (int c = 0; c < Height; c++) {
+			for (int r = 0; r < Width; r++) {
+				if (r < index)
+					tiles[c, r] = this[c, r];
+				else if (r > index)
+					tiles[c, r] = this[c, r + 1];
 			}
 		}
-		Tiles = tiles;
+
+		SetDeepTiles(tiles);
 
 		return true;
 	}
 
 	public bool SwitchTiles(int x1, int y1, int x2, int y2) {
-		if(ColumnCount <= x1 || RowCount <= y1 || ColumnCount <= x2 || RowCount <= y2)
+		if (Height <= x1 || Width <= y1 || Height <= x2 || Width <= y2)
 			return false;
 
-		Tile tile1 = Tiles[x1, y1];
-		Tile tile2 = Tiles[x2, y2];
+		Tile tile1 = this[x1, y1];
+		Tile tile2 = this[x2, y2];
 
-		Tiles[x1, y1] = tile2;
-		Tiles[x2, y2] = tile1;
+		this[x1, y1] = tile2;
+		this[x2, y2] = tile1;
 
 		return true;
 	}
 
 	public bool SwitchColumns(int column1, int column2) {
-		if(ColumnCount <= column1 || ColumnCount <= column2)
+		if (Height <= column1 || Height <= column2)
 			return false;
 
-		for(int r = 0; r < RowCount; r++) {
-			SwitchTiles(column1, r, column2, r);
-		}
+		for (int r = 0; r < Width; r++) SwitchTiles(column1, r, column2, r);
 
 		return true;
 	}
 
 	public bool SwitchRows(int row1, int row2) {
-		if(RowCount <= row1 || RowCount <= row2)
+		if (Width <= row1 || Width <= row2)
 			return false;
 
-		for(int c = 0; c < ColumnCount; c++) {
-			SwitchTiles(c, row1, c, row2);
-		}
+		for (int c = 0; c < Height; c++) SwitchTiles(c, row1, c, row2);
 
 		return true;
 	}
 
-	public int PrioCoreCount() {
-		int count = 0;
-		for(int c = 0; c < ColumnCount; c++) {
-			for(int r = 0; r < RowCount; r++) {
-				if(Tiles[c, r].Type == TileType.PrioCore)
-					count++;
-			}
-		}
-		return count;
-	}
 
 	public bool IsValid() {
 		// Checks if all Tiles are set
-		for(int c = 0; c < ColumnCount; c++) {
-			for(int r = 0; r < RowCount; r++) {
-				if(Tiles[c, r] == null)
+		for (int c = 0; c < Height; c++) {
+			for (int r = 0; r < Width; r++) {
+				if (this[c, r] == null)
 					return false;
 			}
 		}
 
 		// Checks if there is only one priority Beacon
-		if(PrioCoreCount() != 1)
+		if (PrioCoreCount != 1)
 			return false;
 
 		return true;
@@ -217,45 +240,49 @@ public class Map {
 
 	public string ToMapString() {
 		string mapString = "";
-		for(int r = 0; r < RowCount + 1; r++) {
-			for(int c = 0; c < ColumnCount + 1; c++) {
-				if(r == RowCount) {
-					if(c == 0) {
+		for (int r = 0; r < Width + 1; r++) {
+			for (int c = 0; c < Height + 1; c++) {
+				if (r == Width) {
+					if (c == 0)
 						mapString += "╚═══";
-					} else if(c == ColumnCount) {
+					else if (c == Height)
 						mapString += "╝\n";
-					} else {
+					else
 						mapString += "╧═══";
-					}
-				} else {
-					if(r == 0 && c == 0) {
+				}
+				else {
+					if (r == 0 && c == 0)
 						mapString += "╔═══";
-					} else if(r == 0 && c == ColumnCount) {
+					else if (r == 0 && c == Height)
 						mapString += "╗\n";
-					} else if(c == 0) {
+					else if (c == 0)
 						mapString += "╟═══";
-					} else if(c == ColumnCount) {
+					else if (c == Height)
 						mapString += "╢\n";
-					} else if(r == 0) {
+					else if (r == 0)
 						mapString += "╤═══";
-					} else {
+					else
 						mapString += "╬═══";
-					}
 				}
 			}
-			for(int c = 0; c < ColumnCount; c++) {
-				if(r == RowCount) {
-				} else {
-					string value = ((int) Tiles[c, r].Type).ToString();
+
+			for (int c = 0; c < Height; c++) {
+				if (r == Width) {
+				}
+				else {
+					string value = ((int) this[c, r].Type).ToString();
 					mapString += "║";
 					mapString += " ";
 					mapString += value;
 					mapString += new string(' ', 2 - value.Length);
-					if(c == ColumnCount - 1)
+					if (c == Height - 1)
 						mapString += "║\n";
 				}
 			}
 		}
+
 		return mapString;
 	}
+
+	public bool IsWithin(Position newPos) => newPos.X < Width && newPos.Y < Height;
 }
